2b5dfe784066d9429f22152d5c5615508f874292
Nidhi <nidhipanchal1119@gmail.com>
2020-08-31 20:45:39 -0700

    Updated HTML and CSS to create navigation bar & separated blog posts

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Nidhi Panchal',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 31, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 1, 2020'
+    }
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About') # title changes the title in the tab not on the page
 
 
 if __name__ == '__main__':
